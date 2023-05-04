0ed35d7d20743194e142aa70e619cacc001d17f4
Philip Demand <philipdemand@yahoo.com>
2023-05-03 23:04:53 -0400

    commit tree 096c85150bc55451a630beb2a429b0450dabd676

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const mainDiv = document.querySelector("#main")
+mainDiv.remove()
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.innerHTML = "Phil is the champion"
